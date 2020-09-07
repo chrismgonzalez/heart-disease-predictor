413697c95334d5831631509d9aa760c9a648ff5d
Chris Gonzalez <cmgonza89@gmail.com>
2020-09-07 17:02:20 -0500

    Basic page scaffold with routing

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Dashboard from './Dashboard';
-import { BrowserRouter } from "react-router-dom";
+import App from './App';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Dashboard />
-    </BrowserRouter>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
