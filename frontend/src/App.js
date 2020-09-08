73b9c33c942f9c5fb12730f551fd11c2b21786cb
Chris Gonzalez <cmgonza89@gmail.com>
2020-09-07 22:22:27 -0500

    built out form fields

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import { makeStyles } from "@material-ui/core/styles"
+import Home from "./views/Home"
+import About from "./views/About"
+import Predict from "./views/Predict"
+import Analysis from "./views/Analysis"
 
 import {
   BrowserRouter as Router,
@@ -10,11 +14,12 @@ import {
 import {
   Drawer, List, ListItem,
   ListItemIcon, ListItemText,
-  Container, Typography,
 } from "@material-ui/core";
 
 import HomeIcon from "@material-ui/icons/Home";
 import InfoIcon from '@material-ui/icons/Info';
+import SendIcon from '@material-ui/icons/Send';
+import BarChartIcon from '@material-ui/icons/BarChart';
 
 const useStyles = makeStyles((theme) => ({
   drawerPaper: { width: 'inherit' },
@@ -30,7 +35,7 @@ function App() {
     <Router>
       <div style={{ display: 'flex' }}>
         <Drawer
-          style={{ width: '220px' }}
+          style={{ width: '200px' }}
           variant="persistent"
           anchor="left"
           open={true}
@@ -56,7 +61,7 @@ function App() {
             <Link to="/data" className={classes.link}>
               <ListItem button>
                 <ListItemIcon>
-                  <InfoIcon />
+                  <SendIcon />
                 </ListItemIcon>
                 <ListItemText primary={"Predict"} />
               </ListItem>
@@ -64,7 +69,7 @@ function App() {
             <Link to="/analysis" className={classes.link}>
               <ListItem button>
                 <ListItemIcon>
-                  <InfoIcon />
+                  <BarChartIcon />
                 </ListItemIcon>
                 <ListItemText primary={"Analysis"} />
               </ListItem>
@@ -73,44 +78,16 @@ function App() {
         </Drawer>
         <Switch>
           <Route exact path="/">
-            <Container>
-              <Typography variant="h3" gutterBottom>
-                Home
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-              </Typography>
-            </Container>
+            <Home />
           </Route>
           <Route exact path="/about">
-            <Container>
-              <Typography variant="h3" gutterBottom>
-                About
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-              </Typography>
-            </Container>
+            <About/>
           </Route>
           <Route exact path="/data">
-            <Container>
-              <Typography variant="h3" gutterBottom>
-                Enter Health Data
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Information here
-              </Typography>
-            </Container>
+            <Predict />
           </Route>
           <Route exact path="/analysis">
-            <Container>
-              <Typography variant="h3" gutterBottom>
-                Data Analysis
-              </Typography>
-              <Typography variant="body1" gutterBottom>
-                Charts, Graphs, and othe Data here
-              </Typography>
-            </Container>
+            <Analysis />
           </Route>
         </Switch>
       </div>
